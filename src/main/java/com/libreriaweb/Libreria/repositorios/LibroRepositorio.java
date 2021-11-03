f94842115c760dea51fe33ff61ede633873999ea
“Ruben-Escobar” <ra.esco.2108@gmail.com>
2021-11-02 21:10:38 -0300

    Conexion Controlador-Vista OKgit add .!

--- src/main/java/com/libreriaweb/Libreria/repositorios/LibroRepositorio.java
+++ src/main/java/com/libreriaweb/Libreria/repositorios/LibroRepositorio.java
@@ -13,3 +13,22 @@ public interface LibroRepositorio extends JpaRepository<Libro, String> {
     public Libro buscarPorTitulo(@Param("titulo") String titulo);
        
 }
+
+
+
+//OTRA FORMA: 
+//
+//@Repository
+//public interface PersonaRepository extends JpaRepository<Persona, Long> {
+//
+//    @Query("select p from Persona p where p.nombre like %?1%")//busqueda por un solo campo
+//    List<Persona> findByName(String nombre);
+//
+//    /**busqueda por varios campos, lo que se quiere es que cuando ingrese una letra, este se búsque  
+//     * en los dos campos nombre y apellido no por separado la cual lo realiza la siguiente consulta
+//
+//    */
+//    @Query("select p from Persona p where p.nombre = :nombre or p.apellido = :apellido")
+//    List<Persona> findByNameOrLastName(@Param("nombre") String nombre, @Param("apellido") String apellido);//se quiere que reciba solo un parámetro pero que realice la búsqueda en los dos campos
+//
+//}
