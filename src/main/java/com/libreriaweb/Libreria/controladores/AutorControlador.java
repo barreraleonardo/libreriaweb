9a7b88df5d2b08e4259eeec96659890fe38f41b8
Leonardo Barrera <leonardobarrera397@gmail.com>
2021-11-09 22:16:10 -0300

    actualizaciones de templates

--- src/main/java/com/libreriaweb/Libreria/controladores/AutorControlador.java
+++ src/main/java/com/libreriaweb/Libreria/controladores/AutorControlador.java
@@ -2,10 +2,16 @@
 package com.libreriaweb.Libreria.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/autor")
 public class AutorControlador {
+    
+    @GetMapping("/cargarAutor")
+    public String cargar(){
+        return "cargar-autor";
+    }
     
 }
--- src/main/java/com/libreriaweb/Libreria/controladores/EditorialControlador.java
+++ src/main/java/com/libreriaweb/Libreria/controladores/EditorialControlador.java
@@ -2,10 +2,17 @@
 package com.libreriaweb.Libreria.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/editorial")
 public class EditorialControlador {
+    
+    
+   @GetMapping("/guardar")
+   public String guardar(){
+       return "cargar-editorial";
+   }
     
 }
